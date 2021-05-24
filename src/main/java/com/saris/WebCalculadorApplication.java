dc15bdb5fdcaab488b16624c574f4e70164ecde1
Sarissg <l173100333@zacatlan.tecnm.mx>
2021-05-24 15:48:26 -0500

    inicio

---
+++ src/main/java/com/saris/WebCalculadorApplication.java
@@ -0,0 +1,13 @@
+package com.saris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCalculadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCalculadorApplication.class, args);
+	}
+
+}
