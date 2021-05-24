49e50fc49e0a4284509591cbd2edae0391ba0356
Sarissg <l173100333@zacatlan.tecnm.mx>
2021-05-24 16:19:12 -0500

    ultimas modificaciones

--- src/main/java/com/saris/CalculadoraController.java
+++ src/main/java/com/saris/CalculadoraController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class CalculadoraController {
 
-	@GetMapping("/calculadora")
+	@GetMapping("/")
 	public String index(Model modelo)
 	{
 		modelo.addAttribute("calcu", new Calculadora());
